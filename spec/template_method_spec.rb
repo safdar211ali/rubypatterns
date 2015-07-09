86592d5d2b5c93b0e7e77a83acda5378cfa672e3
ali <safdar211@hotmail.com>
2015-07-09 14:38:58 +0500

    mage occupation

--- spec/template_method_spec.rb
+++ spec/template_method_spec.rb
@@ -20,5 +20,19 @@ describe "Template Method Pattern" do
     it "has default damage rating of 15" do
       expect(hero.damage).to eq(15)
     end
+    it "has Strike ability" do
+      expect(hero.abilities).to include(:strike)
+    end
+  end
+
+  context "mage" do
+    let(:hero) {Hero.new :mage}
+
+    it "has default damage rating of 7" do
+      expect(hero.damage).to eq(7)
+    end
+    it "has magic arrow ability" do
+      expect(hero.abilities).to include(:magic_arrow)
+    end
   end
 end
