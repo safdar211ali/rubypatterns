dad24c3c2426143267b93d18d146b5a61e6b6dfd
ali <safdar211@hotmail.com>
2015-07-12 03:35:46 +0500

    inventory class

--- lib/iterator.rb
+++ lib/iterator.rb
@@ -1,11 +1,19 @@
 class Inventory
-
+attr_reader :items
+def initialize
+  @items = []
+end
+  def add(item)
+    @items << item
+  end
 end
 
 class Item
-  attr_reader :cost
+  attr_accessor :cost
 
   def initialize
     @cost = 0
   end
 end
+
+i
